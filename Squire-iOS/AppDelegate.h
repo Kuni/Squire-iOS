b43fa89cbc88c461e2edce6c782dde692fc0257c
CHENHSIN-PANG <BeniMac@CHENHSIN-PANGdeiMac.local>
2015-05-04 09:22:45 +0800

    Initial Commit

---
+++ Squire-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Squire-iOS
+//
+//  Created by CHENHSIN-PANG on 2015/5/4.
+//  Copyright (c) 2015年 CinnamonRoll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Squire-iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Squire-iOS
+//
+//  Created by CHENHSIN-PANG on 2015/5/4.
+//  Copyright (c) 2015年 CinnamonRoll. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
